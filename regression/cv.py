0b2747537843cb5ff1c2b59e5a8f6f8af93e6a0e
Harry <30531325+HarryHy@users.noreply.github.com>
2018-10-27 12:05:59 -0400

    Using timeit to calculate the running time for each loop
    <script.py> output:
        100 loops, best of 3: 10.2 ms per loop
        0.871871278262
        10 loops, best of 3: 30.9 ms per loop
        0.843612862013

--- regression/cv.py
+++ regression/cv.py
@@ -12,3 +12,13 @@ cv_scores = cross_val_score(reg, X, y, cv = 5)
 print(cv_scores)
 
 print("Average 5-Fold CV Score: {}".format(np.mean(cv_scores)))
+
+# Perform 3-fold CV
+cvscores_3 = cross_val_score(reg, X, y, cv = 3)
+%timeit cross_val_score(reg, X, y, cv = 3)
+print(np.mean(cvscores_3))
+
+# Perform 10-fold CV
+cvscores_10 = cross_val_score(reg, X, y, cv = 10)
+%timeit cross_val_score(reg, X, y, cv = 10)
+print(np.mean(cvscores_10))
