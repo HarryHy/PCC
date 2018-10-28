00e0965ce11a64ab42dad3b2051df60e8dc21a02
Harry <30531325+HarryHy@users.noreply.github.com>
2018-10-27 23:29:25 -0400

    Update 06-Hyperparameter tuning with GridSearchCV.py
    Add the instruction part

--- 01-supervised-learning-with-scikit-learn/03-fine-tuning-your-model/06-Hyperparameter tuning with GridSearchCV.py
+++ 01-supervised-learning-with-scikit-learn/03-fine-tuning-your-model/06-Hyperparameter tuning with GridSearchCV.py
@@ -18,3 +18,13 @@ logreg_cv.fit(X,y)
 # Print the tuned parameters and score
 print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_)) 
 print("Best score is {}".format(logreg_cv.best_score_))
+
+'''
+Import LogisticRegression from sklearn.linear_model and GridSearchCV from sklearn.model_selection.
+Setup the hyperparameter grid by using c_space as the grid of values to tune C over.
+Instantiate a logistic regression classifier called logreg.
+Use GridSearchCV with 5-fold cross-validation to tune C:
+Inside GridSearchCV(), specify the classifier, parameter grid, and number of folds to use.
+Use the .fit() method on the GridSearchCV object to fit it to the data X and y.
+Print the best parameter and best score obtained from GridSearchCV by accessing the best_params_ and best_score_ attributes of logreg_cv.
+'''
